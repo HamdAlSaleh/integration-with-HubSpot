933f858b612499038e4850f47b8177cf484fe2a9
Hamd Al Saleh <144883099+HamdAlSaleh@users.noreply.github.com>
2023-10-05 13:15:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,23 +7,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
-
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
+const PRIVATE_APP_ACCESS = 'pat-na1-bede5da6-1688-44de-92eb-a95a27715110';
 
 * * App.get sample
 app.get('/contacts', async (req, res) => {
@@ -64,7 +48,7 @@ app.post('/update', async (req, res) => {
     }
 
 });
-*/
+
 
 
 // * Localhost
